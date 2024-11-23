0782a4f2cd2ef61b6a2dbfd8b472f472ae136e1c
Kanstantsin Mikhaltsevich <k_mikhaltsevich@wargaming.net>
2024-11-23 17:50:22 +0200

    Fix api for creatorId

--- prisma/prisma-actions.ts
+++ prisma/prisma-actions.ts
@@ -1,9 +1,18 @@
 import { prisma } from '@/prisma/prisma-client';
 import { GetTournamentById } from '@/prisma/prisma-types';
+import { auth } from '@/configs/authOptions';
 
 export const getTournamentById: GetTournamentById = async (tournamentId) => {
+    const session = await auth();
+    const creatorId = session?.user.id;
+
+    if (!creatorId) {
+        console.error('User id is missing');
+        return null
+    }
+
     return prisma.tournament.findUnique({
-        where: { id: tournamentId },
+        where: { id: tournamentId, creatorId },
         include: {
             participants: {
                 orderBy: { id: 'asc' },
